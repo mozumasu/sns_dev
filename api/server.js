2b161a81be137375600ecf3960a71290d87bb782
mozumasu <ouri1229@gmail.com>
2023-07-12 18:30:27 +0900

    修正：フロントエンドの環境構築

--- api/server.js
+++ api/server.js
@@ -1,16 +1,7 @@
-//インポート
 const express = require('express');
-app = express();
-const authRoute = require('./routers/auth');
+const app = express();
 
-require('dotenv').config();
-
+//port番号を指定
 const PORT = 5000;
 
-//expressでjson形式を使用する
-app.use(express.json());
-
-// /api/authがエンドポイントの先頭につくようにする
-app.use('/api/auth', authRoute);
-
-app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
