dada15bd5e57b0d274933558bae200b09ec5e19b
mozumasu <ouri1229@gmail.com>
2023-07-19 22:59:50 +0900

    呟き投稿用APIを作成

--- client/src/components/Timeline.tsx
+++ client/src/components/Timeline.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
+import apiClient from '@/lib/apiClient';
 
 const Timeline = () => {
+  const [postText, setPostText] = useState<string>('');
+  //非同期処理
+  const handleSubmi = async (e: React.FormEvent<HTMLFormElement>) => {
+    //リロードを避ける
+    e.preventDefault();
+
+    //呟きAPI呼出し
+    try {
+      await apiClient.post('/posts/post', {
+        content: postText,
+      });
+
+      setPostText('');
+    } catch (err) {
+      alert('ログインしてください。');
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="container mx-auto py-4">
         <div className="bg-white shadow-md rounded p-4 mb-4">
-          <form>
+          <form onSubmit={handleSubmi}>
             <textarea
               className="w-full h-24 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="What's on your mind?"
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setPostText(e.target.value)
+              }
+              //投稿ボタン押下後空にする
+              value={postText}
             ></textarea>
             <button
               type="submit"
